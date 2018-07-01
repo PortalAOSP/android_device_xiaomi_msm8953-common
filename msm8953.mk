e910a78d1a13ce037032d7792237f77e730970a8
mdeejay <mdjrussia@gmail.com>
2018-07-01 08:46:31 +0200

    msm8953-common: Add prebuilt Gcam
    * GoogleCamera-Pixel2Mod-Arnova8G2-V8

--- msm8953.mk
+++ msm8953.mk
@@ -113,7 +113,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
-    Snap \
+    Gcam \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
